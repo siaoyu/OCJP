0b8a544d7d37f854b364f3909747020ffe152b2d
siaoyu <schobitsy@gmail.com>
2017-03-16 21:18:30 +0800

    <siaoyu><modify><>

--- OCJP/src/tw/org/iii/Lsy20170316.java
+++ OCJP/src/tw/org/iii/Lsy20170316.java
@@ -1,11 +1,13 @@
 package tw.org.iii;
 
+import javax.swing.JOptionPane;
+
 public class Lsy20170316 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HELLO");
-		System.out.println("HELLO2");
+		String input = JOptionPane.showInputDialog("input year");
+		int year = Integer.parseInt(input);
+		System.out.println(year+"年");
 	}
 
 }
