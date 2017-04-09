3ee90ac5e72f0788121525c42689ef3f533d4134
siaoyu <schobitsy@gmail.com>
2017-04-09 14:52:49 +0800

    <siaoyu><add><>

--- OCJP/src/tw/org/iii/DigitaSign.java
+++ OCJP/src/tw/org/iii/DigitaSign.java
@@ -1,10 +1,41 @@
 package tw.org.iii;
 
-public class DigitaSign {
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class DigitaSign extends JFrame{
+
+	private JButton clear, undo, redo, save, open;
+	
+	public DigitaSign(){
+		super("Digita Sign");
+		setLayout(new BorderLayout());
+		clear = new JButton("Clear");
+		undo = new JButton("Undo");
+		redo = new JButton("Redo");
+		
+		JPanel top = new JPanel(new FlowLayout());
+		top.add(clear);
+		top.add(undo);
+		top.add(redo);
+		add(top, BorderLayout.NORTH);
+		
+		MySignPanel msp = new MySignPanel();
+//		add(msp, );
+		
+		
+		
+		
+		setSize(640, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new DigitaSign();
 	}
 
 }
